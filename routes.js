d10a2c6e54216a8c0048b7275c2c51821fea5d7f
RMH <riki@craftsmoothie.co.nz>
2019-01-19 11:22:29 +1300

    required fs and path

--- routes.js
+++ routes.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const routes = express.Router()
+
+// tell express how it should use the body of HTTP requests
+routes.use(express.static('public'))
+routes.use(express.urlencoded({extended: false}))
+
+// ??
+// routes.get('/', (req, res) => {
+//   res.redirect('/craftsmoothie')
+// })
 
 routes.get('/craftsmoothie', (req, res) => {
   res.send('Home route is working')
 })
 
 routes.get('/craftsmoothie/shop', (req, res) => {
-  res.send('Shop route is working')
+  res.send('Craft Smoothie shop route is working')
 })
 
 module.exports = routes
